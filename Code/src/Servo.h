b75a9411b01d5263a2fa9b4a33b5f22faf67c6b1
yaremich <58247822+yaremich@users.noreply.github.com>
2020-05-05 18:04:36 +0300

    2 commit:
    functionality for servo

---
+++ Code/src/Servo.h
@@ -0,0 +1,34 @@
+#include "Servo.h"
+
+void ServoInit(void)
+{
+	//75 forward; 61 right; 89 left;
+	GPIO_InitTypeDef port;
+	TIM_TimeBaseInitTypeDef timer; 
+	TIM_OCInitTypeDef timerPWM; 
+
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);	
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
+	
+	port.GPIO_Mode = GPIO_Mode_AF_PP;
+	port.GPIO_Pin = GPIO_Pin_9; 
+	port.GPIO_Speed = GPIO_Speed_50MHz; 
+	GPIO_Init(GPIOB, &port);
+
+	TIM_TimeBaseStructInit(&timer);
+	timer.TIM_ClockDivision = TIM_CKD_DIV1;
+	timer.TIM_CounterMode = TIM_CounterMode_Up;   
+	timer.TIM_Prescaler = 1440-1;
+	timer.TIM_Period = 1000;     
+	TIM_TimeBaseInit(TIM4, &timer);
+
+	TIM_OCStructInit(&timerPWM);
+	timerPWM.TIM_Pulse = 75;   
+	timerPWM.TIM_OCMode = TIM_OCMode_PWM1; 
+	timerPWM.TIM_OutputState = TIM_OutputState_Enable; 
+	timerPWM.TIM_OCPolarity = TIM_OCPolarity_High; 
+	TIM_OC4Init(TIM4,&timerPWM);
+	TIM_OC4PreloadConfig(TIM4, TIM_OCPreload_Enable);
+	TIM_CtrlPWMOutputs(TIM4, ENABLE);
+  TIM_Cmd(TIM4, ENABLE);
+}

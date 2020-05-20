44ae8ed59f40842243040076d5dd203209fda0c3
yaremich <58247822+yaremich@users.noreply.github.com>
2020-05-20 23:22:42 +0300

    4 commit: updating code

--- Code/src/Engine.c
+++ Code/src/Engine.c
@@ -10,10 +10,10 @@ void EngineInit(void)
 	TIM_OCInitTypeDef timerPWM; 
 
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE); 
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
 
 	port.GPIO_Mode = GPIO_Mode_AF_PP;
-	port.GPIO_Pin = GPIO_Pin_9; 
+	port.GPIO_Pin = GPIO_Pin_0; 
 	port.GPIO_Speed = GPIO_Speed_50MHz; 
 	GPIO_Init(GPIOB, &port);
 
@@ -22,23 +22,23 @@ void EngineInit(void)
 	timer.TIM_CounterMode = TIM_CounterMode_Up;   
 	timer.TIM_Prescaler = 1440-1;
 	timer.TIM_Period = 1000;     
-	TIM_TimeBaseInit(TIM4, &timer);
+	TIM_TimeBaseInit(TIM3, &timer);
 
 	TIM_OCStructInit(&timerPWM);
 	timerPWM.TIM_Pulse = 75;   
 	timerPWM.TIM_OCMode = TIM_OCMode_PWM1; 
 	timerPWM.TIM_OutputState = TIM_OutputState_Enable; 
 	timerPWM.TIM_OCPolarity = TIM_OCPolarity_High; 
-	TIM_OC4Init(TIM4,&timerPWM);
-	TIM_OC4PreloadConfig(TIM4, TIM_OCPreload_Enable);
-	TIM_CtrlPWMOutputs(TIM4, ENABLE);
-	TIM_Cmd(TIM4, ENABLE);
+	TIM_OC3Init(TIM3,&timerPWM);
+	TIM_OC3PreloadConfig(TIM3, TIM_OCPreload_Enable);
+	TIM_CtrlPWMOutputs(TIM3, ENABLE);
+	TIM_Cmd(TIM3, ENABLE);
 }
 
 void EngineStart(void)
 {
 	for(int i = 0; i < 99999999; i++);
-	TIM_SetCompare4(TIM4, 79);
+	TIM_SetCompare3(TIM3, 79);
 	for(int i = 0; i < 99999999; i++);
-	TIM_SetCompare4(TIM4, 78);
+	TIM_SetCompare3(TIM3, 78);
 }
